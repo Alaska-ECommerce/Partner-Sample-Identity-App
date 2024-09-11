ab04bd25e29c63d4d4d09bc2216d488334bd91f3
Jared Barneck <Jared.Barneck@AlaskaAir.com>
2024-09-11 14:53:58 -0600

    First check-in.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Partner-Sample-Identity-App
 This is a sample app to help partners understand how to integrate with Alaska Air using Okta.
+
+# To Build & Run
+1. Download and install Visual Studio.
+2. Download and install dotnet 8.
+3. Open the solution (src\PartnerSampleIdentityApp.sln file)
+4. Run the project.
+
---
+++ src/PartnerSampleIdentityApp/Controllers/SamlController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace ExampleSamlConsumer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SamlController : Controller
+    {
+        [HttpPost("Consume")]
+        public IActionResult Consume([FromForm]string samlResponse)
+        {
+            var decodedResponse = Encoding.UTF8.GetString(Convert.FromBase64String(samlResponse));
+            // Specify the content type as 'application/xml'
+            return Content(decodedResponse, "application/xml", Encoding.UTF8);
+        }
+    }
+
+}
